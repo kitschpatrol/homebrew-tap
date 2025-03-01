1f70edaa900644f6dfde9034e50abc0b50e6e0f6
Eric Mika <eric@ericmika.com>
2025-03-01 14:45:21 -0500

    Speediness checksum fix.

--- Casks/custom/speediness.rb
+++ Casks/custom/speediness.rb
@@ -1,6 +1,6 @@
 cask "speediness" do
   version "1.8.0"
-  sha256 "9d2fb9ec7edb4e599c0ee5cd8860afab6f01ae2f7c6045d5cc8c4e466d788b2c"
+  sha256 "296348ebaf605022d95c8a1cce3536963e87f206f7fee5314eeb3a705af84c1e"
 
   # Dynamic URL...
   # See https://github.com/orgs/Homebrew/discussions/5879
