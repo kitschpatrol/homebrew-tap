9af5b8e97fbdab6342d2cbf9fe3ddc43ab40e875
Eric Mika <kitschpatrol@users.noreply.github.com>
2025-03-23 07:02:56 +0000

    mail-archiver-x 7.3.5

--- Casks/custom/mail-archiver-x.rb
+++ Casks/custom/mail-archiver-x.rb
@@ -1,6 +1,6 @@
 cask "mail-archiver-x" do
-  version "7.3.4"
-  sha256 "a5facd7329a2e49da72e8249a6e7b6a1b1b6551674463e9cac8b2bdfe942ac27"
+  version "7.3.5"
+  sha256 "3e67a3948b5307db4ef24b7d17ee5344f242c41126c151be170a91b9cab4c07f"
 
   url "https://mailarchiver.b-cdn.net/MailArchiverX#{version}.dmg",
       verified: "mailarchiver.b-cdn.net/"
