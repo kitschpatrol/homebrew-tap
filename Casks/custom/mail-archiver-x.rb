141c6f227f1d5e70a3617532e43f274c1fb995c5
Eric Mika <eric@ericmika.com>
2025-05-07 14:54:54 -0400

    mail-archiver-x 7.3.7 (#40)

--- Casks/custom/mail-archiver-x.rb
+++ Casks/custom/mail-archiver-x.rb
@@ -1,6 +1,6 @@
 cask "mail-archiver-x" do
-  version "7.3.6"
-  sha256 "b1e308c79ef448f40cc7cc14f78535bab5c5a39e8a874ab04c0252275205366f"
+  version "7.3.7"
+  sha256 "005d169198f0f6d38a51f9ec55a6b8ab80142eb893ce54f3de8b99fd0a257216"
 
   url "https://mailarchiver.b-cdn.net/MailArchiverX#{version}.dmg",
       verified: "mailarchiver.b-cdn.net/"
