a2058df8a524711f2d57bd4ab0c4458e78ff2f5c
Eric Mika <eric@ericmika.com>
2025-08-19 14:40:41 -0400

    plasticity 25.2.5 (#49)
    Co-authored-by: Eric Mika <kitschpatrol@users.noreply.github.com>

--- Casks/custom/plasticity.rb
+++ Casks/custom/plasticity.rb
@@ -1,9 +1,9 @@
 cask "plasticity" do
   arch arm: "arm64", intel: "x64"
 
-  version "25.2.4"
-  sha256 arm:   "3c15189e100ef2ec3b94bd25b040ef12810488c7097049b7d526242dd1c0600d",
-         intel: "6a0dd71c4cc149b21847bb3200f5c6e973f2a21043ddb1ca4df9f873008dc46a"
+  version "25.2.5"
+  sha256 arm:   "03380dcbb3a6a4f16b9698ded28a846c1bcb48652d4748f699262486bfc14331",
+         intel: "3404c1a285a6f8ac95ed16b9f3d83f3fe7bc0a2d892d40f129ea1b2d510c05b4"
 
   url "https://github.com/nkallen/plasticity/releases/download/v#{version}/Plasticity-darwin-#{arch}-#{version}.zip",
       verified: "github.com/nkallen/plasticity/"
