01c16c0bcb3efabf5679cb9f541265e712b1c614
Eric Mika <kitschpatrol@users.noreply.github.com>
2025-09-14 07:02:02 +0000

    photos-workbench 1.5

--- Casks/custom/photos-workbench.rb
+++ Casks/custom/photos-workbench.rb
@@ -1,6 +1,6 @@
 cask "photos-workbench" do
-  version "1.4"
-  sha256 "f9f2de26085b788d7fd1d2b79226fa17992471202dadef364f1c734fc42dd57e"
+  version "1.5"
+  sha256 "799831d30ff212ec8e97ff0c6452fb493f0c5971eae202b97ab3f1d0b32f47dc"
 
   url "https://www.houdah.com/photosWorkbench/download_assets/PhotosWorkbench#{version}.zip"
   name "Photos Workbench"
