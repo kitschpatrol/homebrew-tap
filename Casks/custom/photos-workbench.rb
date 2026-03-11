3d78be85821374d8eb0c4507b08f845284fd9814
Eric Mika <eric@ericmika.com>
2026-03-11 10:59:53 -0400

    photos-workbench 1.6 (#73)
    Co-authored-by: Eric Mika <kitschpatrol@users.noreply.github.com>

--- Casks/custom/photos-workbench.rb
+++ Casks/custom/photos-workbench.rb
@@ -1,6 +1,6 @@
 cask "photos-workbench" do
-  version "1.5"
-  sha256 "799831d30ff212ec8e97ff0c6452fb493f0c5971eae202b97ab3f1d0b32f47dc"
+  version "1.6"
+  sha256 "e9c9970bfe3d3dbf9cff3ec3ebb0a6a36ae03aecc49ee9710bdae3a1cf047eb0"
 
   url "https://www.houdah.com/photosWorkbench/download_assets/PhotosWorkbench#{version}.zip"
   name "Photos Workbench"
