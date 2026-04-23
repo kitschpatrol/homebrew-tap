53a61716750aac677b80e79b40b5fab718440a0f
Eric Mika <kitschpatrol@users.noreply.github.com>
2026-04-23 07:31:38 +0000

    max-messages 1.1.0

--- Casks/custom/max-messages.rb
+++ Casks/custom/max-messages.rb
@@ -1,6 +1,6 @@
 cask "max-messages" do
-  version "1.0.1"
-  sha256 "eafbb8bb0797e10459398872970b468eeeb16298d4b5cb0bbf3c3e1b1bc8d46e"
+  version "1.1.0"
+  sha256 "e836cf9a398b7686c2e9be6ef27e211aae15add6a4a8d2937b50d3eef740ab32"
 
   url "https://mailarchiver.b-cdn.net/MAXMessages#{version}.dmg",
       verified: "mailarchiver.b-cdn.net/"
