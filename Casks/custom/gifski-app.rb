0755b120b80be0903f1b070010b0aa2a4f6079d9
Eric Mika <eric@ericmika.com>
2026-04-30 09:54:40 -0400

    Pin last Sequoia-compatible version of Gifski app. Original gifski-app cask now points to Tahoe-only version.

--- Casks/custom/gifski-app.rb
+++ Casks/custom/gifski-app.rb
@@ -2,21 +2,22 @@ cask "gifski-app" do
   # "Updated once a year" at
   # https://github.com/sindresorhus/Gifski
 
-  version "2.23.0"
-  sha256 "f292b1359e25af0a5b08e620d3c3d319c085beb443d440fbbfc69c1da6126ef3"
+  version "3.0.0"
+  sha256 "11f47f7a27edd7d0867e97f718c7a6dd3579df73a4d9f741f558d546a3cd61c9"
 
   # Version at end of the url works around Homebrew's
   # insistence on skipping checksums on unversioned URLs.
-  url "https://github.com/sindresorhus/meta/files/13539147/Gifski-2.23.0-1692807940.zip##{version}",
-      verified: "github.com/"
+  url "https://www.dropbox.com/scl/fi/2i6entp1fj83zb16al3yf/Gifski-3.0.0-1777056640.zip?rlkey=ealuyayatxef5g61yl9240eyc&raw=1##{version}",
+      verified: "dropbox.com/"
   name "Gifski"
   desc "GUI for Gifski video to gif conversion library"
-  homepage "https://gif.ski/"
+  homepage "https://sindresorhus.com/gifski"
 
   livecheck do
     url "https://github.com/sindresorhus/Gifski"
     strategy :github_latest
+    regex(/^v?(\d+(?:\.\d+)+)$/)
   end
 
-  app "Gifski.app"
+  depends_on macos: ">= :tahoe"
 end
---
+++ Casks/pinned/gifski-app223.rb
@@ -0,0 +1,19 @@
+cask "gifski-app223" do
+  # Last Sequoia-compatible release
+  version "2.23.0"
+  sha256 "bfa4891464b76be9f306a3e8200b7873c70d2ad90575653575993c08793bf364"
+
+  url "https://github.com/sindresorhus/Gifski/releases/download/v#{version}/Gifski.#{version}.-.macOS.13.zip",
+      verified: "github.com/sindresorhus/Gifski/"
+  name "Gifski"
+  desc "GUI for Gifski video to gif conversion library"
+  homepage "https://sindresorhus.com/gifski"
+
+  livecheck do
+    skip "Pinned version"
+  end
+
+  depends_on macos: [">= :ventura", "<= :sequoia"]
+
+  app "Gifski.app"
+end
