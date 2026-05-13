11b56dab53ca524d39ccb19a93bc7966a021bc4e
Eric Mika <kitschpatrol@users.noreply.github.com>
2026-05-13 16:45:02 +0000

    cadova-viewer 0.3.0

--- Casks/custom/cadova-viewer.rb
+++ Casks/custom/cadova-viewer.rb
@@ -1,6 +1,6 @@
 cask "cadova-viewer" do
-  version "0.2.1"
-  sha256 "a809219a0924949e45757b91fe67755fc415b61c76b34825dcc128cfc016eb54"
+  version "0.3.0"
+  sha256 "e0c38512982a2a92edb978dac2c4e7425d814d4acd96d5d3bb2cb35f4ce98707"
 
   url "https://github.com/tomasf/CadovaViewer/releases/download/#{version}/CadovaViewer#{version}.zip"
   name "Cadova Viewer"
