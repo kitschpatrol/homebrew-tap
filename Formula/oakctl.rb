4dab4f2be68ca601a991db78f8ae2fad7b3c752c
Eric Mika <kitschpatrol@users.noreply.github.com>
2026-08-12 08:07:16 +0000

    oakctl 0.27.2

--- Formula/oakctl.rb
+++ Formula/oakctl.rb
@@ -1,8 +1,8 @@
 class Oakctl < Formula
   desc "Command-line tool for managing Luxonis OAK cameras"
   homepage "https://docs.luxonis.com/software-v3/oak-apps/oakctl"
-  url "https://oakctl-releases.luxonis.com/data/0.27.1/darwin_arm64/oakctl"
-  sha256 "0939b6ef0f2cbb16d4fa84870ac05d8d3dd78c1590a1e01d548ff47162f44efd"
+  url "https://oakctl-releases.luxonis.com/data/0.27.2/darwin_arm64/oakctl"
+  sha256 "a5e53d20ab0e3999f281d95889876a7a087f83259f96cd359e57bd84f1dfaaaa"
 
   livecheck do
     url "https://oakctl-releases.luxonis.com/version"
