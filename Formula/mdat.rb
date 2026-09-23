cd573e574f2904037dd53e9a5fbd61f5c00f61bd
Eric Mika <eric@ericmika.com>
2026-09-23 14:18:21 -0400

    mdat 3.3.0

--- Formula/mdat.rb
+++ Formula/mdat.rb
@@ -1,8 +1,8 @@
 class Mdat < Formula
   desc "Markdown Autophagic Template (MDAT) system"
   homepage "https://github.com/kitschpatrol/mdat"
-  url "https://registry.npmjs.org/mdat/-/mdat-3.2.2.tgz"
-  sha256 "dab00becdb673961ee87d730b077688614756b7a554f78f94a6ba38819a2d0c9"
+  url "https://registry.npmjs.org/mdat/-/mdat-3.3.0.tgz"
+  sha256 "f2f92c005fc5e6342eefba1fb3730840a332b1fb4a4a8dde52b56e4da0ed5c3f"
   license "MIT"
 
   depends_on "node"
